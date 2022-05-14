69a1b9cbd3bc491f3eb9b62c72f2fb78f76b6e92
Berke <iamberkeyavas@gmail.com>
2022-05-14 04:31:29 +0300

    idk

---
+++ tankwarclient/client/client.py
@@ -0,0 +1,4 @@
+# This class should have `has-a relationship with gameMenu and game`
+
+class Client: 
+    pass
---
+++ tankwarclient/gameMenu/menu.py
@@ -0,0 +1,6 @@
+# This class is going to be a part of Game class
+# Draw buttons here, Join Game, Create Game etc.
+
+class Menu:
+    pass
+
