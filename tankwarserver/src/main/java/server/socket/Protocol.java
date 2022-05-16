3292c7fb88ed578c192b9ede20c6dbfc4ff2a9c2
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2022-05-16 21:38:40 +0300

    adds minor changes

--- tankwarserver/src/main/java/server/socket/Protocol.java
+++ tankwarserver/src/main/java/server/socket/Protocol.java
@@ -1,31 +1,19 @@
 package server.socket;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Protocol {
-    public int getMessageSize() {
-        return messageSize;
-    }
 
-    public void setMessageSize(int messageSize) {
-        this.messageSize = messageSize;
-    }
-
-    public String getMethodType() {
-        return methodType;
-    }
-
-    public void setMethodType(String methodType) {
-        this.methodType = methodType;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    private int messageSize;
+    private Integer messageSize;
     private String methodType;
     private String message;
+    private String status;
+
 }
