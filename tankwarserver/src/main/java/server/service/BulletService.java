83d377e2a2212669159d35d186d403ebc2b48396
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2022-05-16 21:38:11 +0300

    adds minor changes for conventions

--- tankwarserver/src/main/java/server/service/BulletService.java
+++ tankwarserver/src/main/java/server/service/BulletService.java
@@ -5,20 +5,23 @@ import server.model.dto.Bullet;
 import server.utilization.Pair;
 
 public class BulletService {
+
     private InMemoryDao inMemoryDao = InMemoryDao.getInstance();
+
     public Bullet createOrUpdateBullet(Bullet bullet){
-        inMemoryDao.bullets.forEach((n) -> {
-            if(n.getSecond().getPlayerId().equals(bullet.getPlayerId()) && n.getSecond().getBulletId().equals(bullet.getBulletId())) {
-                inMemoryDao.bullets.remove(n);
+        inMemoryDao.bullets.forEach((element) -> {
+            if(element.getSecond().getPlayerId().equals(bullet.getPlayerId()) && element.getSecond().getBulletId().equals(bullet.getBulletId())) {
+                inMemoryDao.bullets.remove(element);
             }
         });
         inMemoryDao.bullets.add(new Pair<>(bullet.getPlayerId(), bullet));
         return bullet;
     }
+
     public void removeBullets(Integer playerId){
-        inMemoryDao.bullets.forEach((n) -> {
-            if(n.getFirst().equals(playerId))
-                inMemoryDao.bullets.remove(n);
+        inMemoryDao.bullets.forEach((element) -> {
+            if(element.getFirst().equals(playerId))
+                inMemoryDao.bullets.remove(element);
         });
     }
 }
