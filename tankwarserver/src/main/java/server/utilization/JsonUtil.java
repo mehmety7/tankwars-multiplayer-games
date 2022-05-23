9fedc547428e13ea6f1117fc0a50724dfeb13fe7
Mehmet Yazıcı <mehmetyazici.3407@gmail.com>
2022-05-24 00:39:25 +0300

    adds convert string list to object list feature

--- tankwarserver/src/main/java/server/utilization/JsonUtil.java
+++ tankwarserver/src/main/java/server/utilization/JsonUtil.java
@@ -2,14 +2,18 @@ package server.utilization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtil {
 
     private static Gson gson = new Gson();
 
     public static String toJson(Object object) {
-        GsonBuilder builder = new GsonBuilder();
-        builder.serializeNulls();
+        GsonBuilder builder = new GsonBuilder().serializeNulls();
         gson = builder.create();
         return gson.toJson(object);
     }
@@ -18,6 +22,14 @@ public class JsonUtil {
         GsonBuilder builder = new GsonBuilder();
         gson = builder.create();
         return gson.fromJson(json, tClass);
+    }
+
+    public static <T> List<T> fromListJson(String jsonList) {
+        GsonBuilder builder = new GsonBuilder();
+        gson = builder.create();
+
+        Type listType = new TypeToken<ArrayList<T>>() {}.getType();
+        return gson.fromJson(jsonList, listType);
     }
 
 }
