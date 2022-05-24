ed5ad1ba31eb86d049b73c5799d4cdf68bc831f3
Omer213447 <omer.erten.7792@hotmail.com>
2022-05-24 20:09:54 +0300

    Refactoring Bullet Model

--- tankwarserver/src/main/java/server/model/dto/Bullet.java
+++ tankwarserver/src/main/java/server/model/dto/Bullet.java
@@ -9,15 +9,17 @@ import server.model.enumerated.FaceOrientation;
 import java.util.UUID;
 /* Tanklar ateş ederken mermilerin çarptığı yere göre serverin aksiyon alması gerek,
 *  Bu yüzden bir mermi sınıfı oluşturdum.*/
+// Mermiler sınırsız hızda düz bir çizgi olacaklarından refactor atıyorum.
+// Shoot komutunda client bir uuid oluşturup yollamalı. Veya biz 1 den başlayıp artacak şekilde integer id yapalım
+// ve shoot komutunda OK responsunda bu id'yi dönelim
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Bullet {
-    private Integer playerId;
+    private Integer tankId;
     private UUID bulletId;
     private Integer positionX;
     private Integer positionY;
     private FaceOrientation faceOrientation;
-    private Double movementSpeed;
 }
