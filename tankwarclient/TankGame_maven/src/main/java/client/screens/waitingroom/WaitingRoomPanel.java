458e39551e355623a1483b61a00172d57eb1d8b4
EnverUsta <enveruusta@gmail.com>
2022-05-28 17:45:47 +0300

    create waiting room panel ui

--- tankwarclient/TankGame_maven/src/main/java/client/screens/waitingroom/WaitingRoomPanel.java
+++ tankwarclient/TankGame_maven/src/main/java/client/screens/waitingroom/WaitingRoomPanel.java
@@ -1,6 +1,12 @@
 package client.screens.waitingroom;
 
+import client.model.dto.Game;
+import client.services.SingletonSocketService;
+import client.socket.ClientSocket;
+
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,13 +19,18 @@ public class WaitingRoomPanel extends JPanel {
     Integer gameId;
     JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 
-    JPanel playersPanel = new JPanel(new BoxLayout(parentPanel, BoxLayout.Y_AXIS));
+    JPanel bodyPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
+    JPanel playersPanel = new JPanel();
+    JPanel gameDetailPanel = new JPanel();
     JLabel waitingRoomTitle = new JLabel();
     JButton backToLobbyBtn = new JButton("Back to Lobby");
+    JButton startGameBtn = new JButton("Start the Game");
 
     public WaitingRoomPanel(JPanel parentPanel, Integer playerId, Integer gameId) {
-        System.out.println("playerId: " + playerId);
-        System.out.println("gameId: " + gameId);
+        ClientSocket cs = SingletonSocketService.getInstance().clientSocket;
+        cs.sendMessage("GG", new Game(gameId, null, null, null, null, null));
+        System.out.println("Server response: " + cs.response());
+
         this.parentPanel = parentPanel;
         this.playerId = playerId;
         this.gameId = gameId;
@@ -32,20 +43,59 @@ public class WaitingRoomPanel extends JPanel {
                 cardLayout.show(parentPanel, "lobbyPanel");
             }
         });
+        startGameBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
 
         addToMainPanel();
-        addToHeaderPanel(backToLobbyBtn, waitingRoomTitle);
+        addToHeaderPanel();
+        addToBodyPanel();
         setWaitingRoomTitle();
     }
 
     private void addToMainPanel() {
+        this.setLayout(new GridLayout(2, 1));
         this.add(headerPanel);
+        this.add(bodyPanel);
     }
 
-    private void addToHeaderPanel(JButton backButton, JLabel title) {
-        headerPanel.add(backButton);
+    private void addToBodyPanel() {
+        bodyPanel.add(playersPanel);
+        bodyPanel.add(Box.createHorizontalStrut(75));
+        bodyPanel.add(gameDetailPanel);
+        addToPlayersPanel();
+        addToGameDetailPanel();
+    }
+
+    private void addToGameDetailPanel() {
+        gameDetailPanel.setLayout(new BoxLayout(gameDetailPanel, BoxLayout.Y_AXIS));
+        gameDetailPanel.add(startGameBtn);
+    }
+
+    private void addToPlayersPanel() {
+        playersPanel.setLayout(new BoxLayout(playersPanel, BoxLayout.Y_AXIS));
+        List<String> mockPlayers = new ArrayList<>(List.of("test1", "test2", "test3"));
+        for (String player : mockPlayers) {
+            JLabel playerLabel = new JLabel(player);
+            playerLabel.setBackground(Color.BLUE);
+            playerLabel.setOpaque(true);
+            playerLabel.setForeground(Color.white);
+            playerLabel.setBorder(new EmptyBorder(0, 10, 0, 0));
+            playerLabel.setMinimumSize(new Dimension(350, 30));
+            playerLabel.setPreferredSize(new Dimension(350, 30));
+            playerLabel.setMaximumSize(new Dimension(350, 30));
+            playersPanel.add(playerLabel);
+            playersPanel.add(Box.createVerticalStrut(5));
+        }
+    }
+
+    private void addToHeaderPanel() {
+        headerPanel.add(backToLobbyBtn);
         headerPanel.add(Box.createHorizontalStrut(100));
-        headerPanel.add(title);
+        headerPanel.add(waitingRoomTitle);
     }
 
     private void setWaitingRoomTitle() {
