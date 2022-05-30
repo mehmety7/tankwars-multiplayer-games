3f0eaad156716321bf288f63fd3de50d6a86c961
alihannatas <alihan2644@gmail.com>
2022-05-30 21:26:37 +0300

    refactors directories

---
+++ tankwarclient/TankGame_maven/src/main/java/client/Home.java
@@ -0,0 +1,54 @@
+package client;
+
+import client.screens.about.AboutUsPanel;
+import client.screens.auth.LoginPanel;
+import client.screens.auth.SignupPanel;
+import client.screens.leadership.LeadershipPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Home {
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Tank Wars");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setBackground(new Color(237,237,237));
+
+        // This JPanel is the base for CardLayout for other JPanels.
+        final JPanel basePanel = new JPanel();
+        basePanel.setLayout(new CardLayout(20, 20));
+
+        /* Here we be making objects of the Window Series classes
+         * so that, each one of them can be added to the JPanel
+         * having CardLayout.
+         */
+
+        //auth panels
+        LoginPanel loginPanel = new LoginPanel(basePanel);
+        basePanel.add(loginPanel, "loginPanel");
+        SignupPanel signupPanel = new SignupPanel(basePanel);
+        basePanel.add(signupPanel, "signupPanel");
+
+        //about us panel
+        AboutUsPanel aboutUsPanel = new AboutUsPanel(basePanel);
+        basePanel.add(aboutUsPanel, "aboutUsPanel");
+
+
+
+
+        // Adding the basePanel to JFrame.
+        frame.add(basePanel, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String... args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
